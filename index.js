acc3aa48a20fa0b6d9a8a256b7814dd8b2d3105a
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-24 15:16:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n) {
+  
+}
