9b5d836b65eb5318524f029ba7ccdb76cc8e2064
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-24 16:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 function increaseRankBy(n) {
-  var lis = document.getElementsById('app').querySelectorAll('ul.ranked-list li');
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
     lis[i]= (parseInt(lis[i])+parseInt(n )).toString
   }
