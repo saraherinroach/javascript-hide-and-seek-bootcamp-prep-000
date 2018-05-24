39cbf3f1b63059dd0d37bad1b61c0597bd84c15e
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-24 16:46:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML= parseInt(lis[i].innerHTML)+parseInt(n )
+  }
+}
+function deepestChild() {
+  var lis = document.getElementById('grand-node').querySelectorAll('div');
+  return lis[lis.length-1]
+}
